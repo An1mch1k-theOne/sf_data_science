873e0041143e0af2deb94aefced1eeae95cc3f84
An1mch1k-theOne <kerambitbambit@yandex.ru>
2024-11-11 01:45:15 +0300

    modern

--- project_0/game.py
+++ project_0/game.py
@@ -11,11 +11,11 @@ while True:
     predict_number = int(input("Угадай число от 1 до 100: "))
     
     if predict_number > number:
-        print("Ваше число больше, чем загаданное")
+        print("Ваше число больше, чем загаданное.")
         
     elif predict_number < number:
-        print("Ваше число меньше, чем загаданное")
+        print("Ваше число меньше, чем загаданное.")
         
     elif predict_number == number:
-        print(f"Вы угадали число {number} за {count} попыток")
+        print(f"Вы угадали число {number} за {count} попыток!")
         break
