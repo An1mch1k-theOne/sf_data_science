5471015ee25d5782852db6fb93f9110fd5260b09
An1mch1k <75303399+An1mch1k-theOne@users.noreply.github.com>
2024-11-11 00:40:56 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,21 @@
+'''Игра угадай число'''
+import numpy as np
+
+number = np.random.randint(1, 100)
+
+# Количество попыток
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    
+    if predict_number > number:
+        print("Ваше число больше, чем загаданное")
+        
+    elif predict_number < number:
+        print("Ваше число меньше, чем загаданное")
+        
+    elif predict_number == number:
+        print(f"Вы угадали число {number} за {count} попыток")
+        break
