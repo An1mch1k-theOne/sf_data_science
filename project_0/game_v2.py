8f317f03428b4f6a1b8920f4d73037de5cd9b917
An1mch1k-theOne <kerambitbambit@yandex.ru>
2024-11-11 03:23:18 +0300

    edit

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -12,6 +12,7 @@ def random_predict(number:int=1) -> int:
     Returns:
         int: Число попыток
     """
+    
     count = 0
     
     while True:
@@ -31,6 +32,7 @@ def score_game(random_predict) -> int:
     Returns:
         int: Среднее количество попыток
     """
+    
     count_ls = []
     np.random.seed(1)
     random_array = np.random.randint(1, 101, size=(1000))
@@ -42,6 +44,37 @@ def score_game(random_predict) -> int:
     print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
+def fast_predict(number: int = 1) -> int:
+    """Угадываем число меньше, чем за 20 попыток, в среднем за 5 попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0
+    low = 1
+    high = 100
+    
+    while True:
+        count += 1
+        
+        # Предполагаемое число - среднее между low и high
+        predict_number = (low + high) // 2  
+        
+        if number == predict_number:
+            break
+        elif number < predict_number:
+            high = predict_number - 1
+        else:
+            low = predict_number + 1
+            
+    return count
+
 # if __name__ == '__main___': #Для импорта
+
 # RUN
-score_game(random_predict)    
+# score_game(random_predict)
+score_game(fast_predict)
